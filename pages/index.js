0052118ffbfd862886e6cc8bb2f322aafe95d174
Ryan Riginding <ryan@riginding.com>
2019-04-19 08:59:03 +0200

    add vova

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
 import {
   BarChart, Bar, YAxis, XAxis, LabelList, ResponsiveContainer
 } from 'recharts';
@@ -21,27 +22,24 @@ const Index = (props) => (
           <LabelList dataKey="totalXP" position="insideRight" />
         </Bar>
       </BarChart>
-)
+);
 
 Index.getInitialProps = async function() {
-  const userNames = ['riginding', 'madfist', 'theanswer']
-  const fetchData = await Promise.all(userNames.map(user => fetch(`https://codestats.net/api/users/${user}`)))
-  let userStats = await Promise.all(fetchData.map((userData) => { 
-    return userData.json()
-  }))
+  const userNames = ['riginding', 'madfist', 'theanswer', 'vosamoilenko'];
+  const fetchData = await Promise.all(userNames.map(user => fetch(`https://codestats.net/api/users/${user}`)));
+  let userStats = await Promise.all(fetchData.map((userData) => {
+      return userData.json();
+  }));
   userStats = userStats.map(stat => {
     return {
       name: stat.user,
       totalXP: stat.total_xp
-    }
+    };
   }).sort((apple, banana) => {
-    return banana.totalXP - apple.totalXP
-  })
-   
+      return banana.totalXP - apple.totalXP;
+  });
 
-  return {
-    data: userStats
-  }
-}
+  return {data: userStats};
+};
 
 export default Index
